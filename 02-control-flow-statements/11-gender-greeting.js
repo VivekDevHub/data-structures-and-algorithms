95c5f3e91bd6e26af78045b9511cf8aa8d5fa0e6
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-08-03 22:58:13 +0530

    gender greeting

---
+++ 02-control-flow-statements/11-gender-greeting.js
@@ -0,0 +1,9 @@
+let gender = "Male";
+
+if (gender === "Male") {
+    console.log("Good Morning, Sir!");
+} else if (gender === "Female") {
+    console.log("Good Morning, Ma'am!");
+} else {
+    console.log("Good Morning!");
+}
