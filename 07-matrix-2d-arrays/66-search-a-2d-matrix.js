096ad951f3f5970f815b347f66e957029e124918
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-08-18 23:02:43 +0530

    search 2d matrix

---
+++ 07-matrix-2d-arrays/66-search-a-2d-matrix.js
@@ -0,0 +1,31 @@
+const matrix = [
+  [10, 20, 30],
+  [40, 50, 60],
+  [70, 80, 90]
+];
+
+const target = 50;
+
+let found = false;
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+
+    if (matrix[i][j] === target) {
+      console.log(`Element found at row ${i}, column ${j}`);
+      found = true;
+    }
+
+  }
+}
+
+if (!found) {
+  console.log("Element not found");
+}
+return;
+
+// i → row ko traverse karega
+// j → column ko traverse karega
+// matrix[i][j] → current element
+// matrix[i][j] === target → check karega ki element mila ya nahi
+
